ddda149556a161f8152e37fdc315ac961356c7e4
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2022-05-13 14:45:04 +0100

    passage des contacts stockés en dur vers le composant ContactList grace aux props

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,23 @@ import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
+  const contacts = [
+    {
+      id: "1",
+      name: "Jorge",
+      email: "jorge@gmail.com",
+    },
+    {
+      id: "2",
+      name: "Jorge2",
+      email: "jorge2@gmail.com",
+    },
+  ];
   return (
     <div className="ui container">
       <Header />
       <AddContact />
-      <ContactList />
+      <ContactList contacts={contacts} />
     </div>
   );
 }
